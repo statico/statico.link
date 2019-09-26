6583db4509b2d114cc6bcb70bba99805568e1e58
Ian Langworth <ian@langworth.com>
2019-09-25 22:24:25 -1000

    Missing viewport meta tag

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const server = http.createServer((req, res) => {
         <title>${TITLE}</title>
         <meta charset="utf-8" />
         <meta name="robots" content="noindex, nofollow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
       </head>
       <body>
