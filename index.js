1b64df7a415d35ea0ae43abc6fd9acca2bf7919a
Ian Langworth <ian@langworth.com>
2022-11-30 21:04:19 -0800

    Rewrite using Prettier defaults

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
-const express = require("express")
-const fs = require("fs")
-const fetch = require("isomorphic-unfetch")
+const express = require("express");
+const fs = require("fs");
+const fetch = require("isomorphic-unfetch");
 
-require("dotenv").config()
+require("dotenv").config();
 
-const PORT = Number(process.env.PORT) || 8080
-const IPDATA_KEY = String(process.env.IPDATA_KEY)
-const LINKS_CONF = process.env.LINKS_CONF || "Missing LINKS_CONF"
-const BASE_URL = process.env.BASE_URL || "https://cool.link"
-const TITLE = process.env.TITLE || "My Awesome Links"
+const PORT = Number(process.env.PORT) || 8080;
+const IPDATA_KEY = String(process.env.IPDATA_KEY);
+const LINKS_CONF = process.env.LINKS_CONF || "Missing LINKS_CONF";
+const BASE_URL = process.env.BASE_URL || "https://cool.link";
+const TITLE = process.env.TITLE || "My Awesome Links";
 const DESCRIPTION =
-  process.env.DESCRIPTION || "A collection of links that I like"
+  process.env.DESCRIPTION || "A collection of links that I like";
 const GITHUB_URL =
-  process.env.GITHUB_URL || "https://github.com/statico/statico-link-list"
+  process.env.GITHUB_URL || "https://github.com/statico/statico-link-list";
 
 const CUSTOM_LINKS = {
   ip: "Your public IPv4 address",
   geoip: "Information about your IP using ipdata.co",
   useragent: "Your current user agent",
-}
+};
 
-const app = express()
-app.set("json spaces", 2)
+const app = express();
+app.set("json spaces", 2);
 
-const ip = (req) => req.headers["x-forwarded-for"] ?? req.socket.remoteAddress
+const ip = (req) => req.headers["x-forwarded-for"] ?? req.socket.remoteAddress;
 
 app.get("/ip", (req, res) => {
-  res.set("Content-Type", "text/plain")
-  res.send(ip(req) + "\n")
-})
+  res.set("Content-Type", "text/plain");
+  res.send(ip(req) + "\n");
+});
 
 app.get("/geoip", async (req, res) => {
   if (IPDATA_KEY) {
     const data = await fetch(
       `https://api.ipdata.co/${ip(req)}?api-key=${IPDATA_KEY}`
-    )
-    const obj = await data.json()
-    res.json(obj)
+    );
+    const obj = await data.json();
+    res.json(obj);
   } else {
-    res.send("IPDATA_KEY not configured")
+    res.send("IPDATA_KEY not configured");
   }
-})
+});
 
 app.get("/useragent", (req, res) => {
-  res.set("Content-Type", "text/plain")
-  res.send(req.headers["user-agent"] + "\n")
-})
+  res.set("Content-Type", "text/plain");
+  res.send(req.headers["user-agent"] + "\n");
+});
 
 app.get("/:key", (req, res) => {
-  const conf = fs.readFileSync(LINKS_CONF, "utf8")
-  const lines = conf.split(/\n+/g).filter((x) => x)
-  const key = req.params.key.replace(/\W/g, "").toLowerCase()
+  const conf = fs.readFileSync(LINKS_CONF, "utf8");
+  const lines = conf.split(/\n+/g).filter((x) => x);
+  const key = req.params.key.replace(/\W/g, "").toLowerCase();
 
   for (const line of lines) {
-    const [path, url] = line.split(/\s+/, 2)
+    const [path, url] = line.split(/\s+/, 2);
     if (path.substr(1) === key) {
-      res.set("Referrer-Policy", "unsafe-url")
-      res.set("X-Robots-Tag", "noindex, nofollow")
-      res.redirect(302, url)
-      return
+      res.set("Referrer-Policy", "unsafe-url");
+      res.set("X-Robots-Tag", "noindex, nofollow");
+      res.redirect(302, url);
+      return;
     }
   }
 
-  res.status(404).send("Not found")
-})
+  res.status(404).send("Not found");
+});
 
 app.get("/", (req, res) => {
   let out = `<!doctype html>
@@ -88,10 +88,10 @@ app.get("/", (req, res) => {
       </div>
       <div class="table-responsive">
       <table class="table table-bordered">
-  `
+  `;
 
   for (const key in CUSTOM_LINKS) {
-    const short = `${BASE_URL}/${key}`
+    const short = `${BASE_URL}/${key}`;
     out += `
       <tr>
         <td class="text-nowrap">
@@ -101,16 +101,16 @@ app.get("/", (req, res) => {
           ${CUSTOM_LINKS[key]}
         </td>
       </tr>
-    `
+    `;
   }
 
-  const conf = fs.readFileSync(LINKS_CONF, "utf8")
-  const lines = conf.split(/\n+/g).filter((x) => x)
+  const conf = fs.readFileSync(LINKS_CONF, "utf8");
+  const lines = conf.split(/\n+/g).filter((x) => x);
   for (const line of lines) {
-    const [path, url, ...more] = line.split(/\s+/)
-    const short = `${BASE_URL}${path}`
-    if (/priv/.test(more.join(" "))) continue
-    if (CUSTOM_LINKS[path.substr(1)]) continue
+    const [path, url, ...more] = line.split(/\s+/);
+    const short = `${BASE_URL}${path}`;
+    if (/priv/.test(more.join(" "))) continue;
+    if (CUSTOM_LINKS[path.substr(1)]) continue;
     out += `
       <tr>
         <td class="text-nowrap">
@@ -129,7 +129,7 @@ app.get("/", (req, res) => {
           >${url}</a>
         </td>
       </tr>
-    `
+    `;
   }
 
   out += `
@@ -141,10 +141,10 @@ app.get("/", (req, res) => {
     
     </body>
     </html>
-  `
-  res.send(out)
-})
+  `;
+  res.send(out);
+});
 
 app.listen(PORT, () => {
-  console.info(`Listening on http://127.0.0.1:${PORT}`)
-})
+  console.info(`Listening on http://127.0.0.1:${PORT}`);
+});
